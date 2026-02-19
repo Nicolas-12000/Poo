d0aac4f18620b4c04c580d648b0a3fc1f7deced6
nicolas <nikolasg12000@gmail.com>
2026-02-19 17:09:00 -0500

    first commit

---
+++ orders-api/orders-api/src/test/java/com/example/orders_api/OrdersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orders_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
